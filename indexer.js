bcf4d5c06cf541306e5585e7a4e398a0e2f36b0b
Martin Munro <mmunro@ltrr.arizona.edu>
2019-01-31 11:04:33 -0700

    Explore tracking parents and siblings.

--- indexer.js
+++ indexer.js
@@ -4,25 +4,30 @@ function paddedname (name, indent) {
   return ' '.repeat(indent) + name;
 }
 
-function formatentry (root, indent) {
+function formatentry (root, parents, siblings, indent) {
   return (entry) => {
     if (entry.isFile()) {
       return paddedname(entry.name, indent);
     }
     if (entry.isDirectory()) {
-      return mappeddir(root, entry.name, indent);
+      return mappeddir(root, entry.name, parents, siblings, indent);
     }
   }
 }
 
-function mappeddir (root, dirname, indent) {
-  const path = root + '/' + dirname;
-  const entryformat = formatentry(path, indent + 2);
-  return fsPromises.readdir(path, { withFileTypes: true })
-  .then(entries => entries.map(entryformat))
+function mappeddir (root, dirname, parents, siblings, indent) {
+  const nextroot = `${root}/${dirname}`;
+  const nextparents = parents.concat([dirname]);
+  const nextindent = indent + 2;
+  return fsPromises.readdir(nextroot, { withFileTypes: true })
+  .then(entries => {
+    const nextsiblings = entries.map(entry => entry.name);
+    const entryformat = formatentry(nextroot, nextparents, nextsiblings, nextindent);
+    return entries.map(entryformat)
+  })
   .then(dirlist => Promise.all(dirlist))
-  .then(subtree => [paddedname(dirname, indent) + '/'].concat(subtree).join("\n"))
+  .then(subtree => [`${paddedname(dirname, indent)}/ ${parents.join(' --> ')} ${siblings.toString}`].concat(subtree).join("\n"))
   .catch(errmes => console.log(errmes));
 }
 
-mappeddir('.', 'home', 0).then(dir => console.log(dir));
+mappeddir('.', 'home', [], [], 0).then(dir => console.log(dir));
