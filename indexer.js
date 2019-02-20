b302e5c57d542ac1cf50bbb6013b51fd92a9952a
Martin Munro <mmunro@ltrr.arizona.edu>
2019-02-19 18:37:15 -0700

    Rework logging.

--- indexer.js
+++ indexer.js
@@ -8,8 +8,6 @@ const destination = process.env.DIRINDEX_DESTINATION || path.resolve('build');
 const webSiteName = process.env.DIRINDEX_SITENAME || 'Home';
 const indexName = process.env.DIRINDEX_INDEXNAME || 'index.html';
 const webRootURL = process.env.DIRINDEX_WEBROOT || url.pathToFileURL(destination);
-// eslint-disable-next-line no-console, no-unused-vars
-const logMessage = (process.env.DIRINDEX_QUIET) ? (mess => undefined) : (mess => console.log(mess));
 const webExtraRoot = ['dirindex'];
 const webScripts = webExtraRoot.concat(['scripts']);
 const webImages = webExtraRoot.concat(['images']);
@@ -27,6 +25,59 @@ const baseOptions = {
   heightFileIcon: '32px',
 };
 
+function VerboseLogger(level) {
+  this.indentLevel = level || 0;
+}
+
+VerboseLogger.prototype.message = function emitLogMessage(mess) {
+  // eslint-disable-next-line no-console
+  console.log(mess);
+};
+
+VerboseLogger.prototype.paddedName = function spacePaddedName(name) {
+  return ' '.repeat(this.indentLevel) + name;
+};
+
+VerboseLogger.prototype.formattedEntry = function formattedDirEntry(name) {
+  return this.paddedName(name);
+};
+
+VerboseLogger.prototype.formattedDir = function formattedDirectory(
+  dirName, parents, siblings, entries,
+) {
+  const dirHeader = `${this.paddedName(dirName)}/ ${parents.join(' --> ')} | [${siblings.join(', ')}]`;
+  return [dirHeader].concat(entries).join('\n');
+};
+
+VerboseLogger.prototype.deeper = function loggerForDeeperLevel() {
+  return new VerboseLogger(this.indentLevel + 2);
+};
+
+function NormalLogger(level) {
+  this.indentLevel = level || 0;
+}
+
+NormalLogger.prototype.message = function emitLogMessage() {};
+
+NormalLogger.prototype.paddedName = function spacePaddedName() {
+  return '';
+};
+
+NormalLogger.prototype.formattedEntry = function formattedDirEntry() {
+  return '';
+};
+
+NormalLogger.prototype.formattedDir = function formattedDirectory() {
+  return '';
+};
+
+NormalLogger.prototype.deeper = function loggerForDeeperLevel() {
+  return this;
+};
+
+const topLog = (process.env.DIRINDEX_QUIET)
+  ? (new NormalLogger()) : (new VerboseLogger());
+
 const errorExit = function forceUntidyErrorExit(message) {
   throw Error(message);
 };
@@ -34,11 +85,7 @@ const errorExit = function forceUntidyErrorExit(message) {
 const subTreeIndex = pug.compileFile(path.join(webAssetDir, 'subtreeindex.pug'), { basedir: assets });
 const topLevelIndex = pug.compileFile(path.join(webAssetDir, 'toplevelindex.pug'), { basedir: assets });
 
-const paddedName = function whiteSpacePaddedName(name, indentLevel) {
-  return ' '.repeat(indentLevel) + name;
-};
-
-const patternCopiedFiles = function copiedFilesMatchingRegExp(srcDir, dstDir, pattern) {
+const patternCopiedFiles = function copiedFilesMatchingRegExp(srcDir, dstDir, pattern, log) {
   return fsPromises.readdir(srcDir, { withFileTypes: true })
     .then(dirents => dirents.filter(entry => entry.isFile() && pattern.test(entry.name))
       .map((fileEntry) => {
@@ -47,7 +94,7 @@ const patternCopiedFiles = function copiedFilesMatchingRegExp(srcDir, dstDir, pa
         return fsPromises.copyFile(src, dst);
       }))
     .then(copyList => Promise.all(copyList))
-    .catch(errmes => logMessage(errmes));
+    .catch(errmes => log.message(errmes));
 };
 
 const joinedPath = function pathStringFromJoinedArray(root, pathList) {
@@ -57,26 +104,26 @@ const joinedPath = function pathStringFromJoinedArray(root, pathList) {
   );
 };
 
-const purgedTree = function recursivelyDeletedDirectoryTree(victim) {
+const purgedTree = function recursivelyDeletedDirectoryTree(victim, log) {
   return fsPromises.readdir(victim, { withFileTypes: true })
     .then(entries => entries.map((entry) => {
       const entryPath = path.join(victim, entry.name);
       if (entry.isDirectory()) {
-        return purgedTree(entryPath);
+        return purgedTree(entryPath, log.deeper());
       }
       return fsPromises.unlink(entryPath);
     }))
     .then(hitList => Promise.all(hitList))
     .then(() => fsPromises.rmdir(victim))
-    .catch(errmes => logMessage(errmes))
+    .catch(errmes => log.message(errmes))
     .then(() => fsPromises.stat(victim))
     .then(
       () => errorExit(`Directory still exists: ${victim}`),
-      () => logMessage(`Deleted OK: ${victim}`),
+      () => log.message(`Deleted OK: ${victim}`),
     );
 };
 
-const relinkedTree = function recursivelyFixedSymbolicLinkTree(srcTree, dstTree) {
+const relinkedTree = function recursivelyFixedSymbolicLinkTree(srcTree, dstTree, log) {
   return fsPromises.readdir(srcTree, { withFileTypes: true })
     .then(entries => entries.map((entry) => {
       const entryPath = path.join(srcTree, entry.name);
@@ -99,39 +146,39 @@ const relinkedTree = function recursivelyFixedSymbolicLinkTree(srcTree, dstTree)
             }
             return `Skipped ${rel}`;
           })
-          .catch(errmes => logMessage(errmes));
+          .catch(errmes => log.message(errmes));
       }
       return `Ignored ${entry.name}`;
     }))
     .then(fixList => Promise.all(fixList))
     .then(logs => logs.join('\n'))
-    .catch(errmes => logMessage(errmes));
+    .catch(errmes => log.message(errmes));
 };
 
-const linkProcessor = function symbolicLinkEntryProcessor(srcTree, dstTree) {
+const linkProcessor = function symbolicLinkEntryProcessor(srcTree, dstTree, log) {
   return name => fsPromises.realpath(path.join(srcTree, name))
     .then(real => fsPromises.symlink(real, path.join(dstTree, name)))
-    .catch(errmes => logMessage(errmes));
+    .catch(errmes => log.message(errmes));
 };
 
 const entryProcessor = function directoryEntryProcessor(
-  srcTree, dstTree, parents, siblings, indentLevel, processedLink, processedDir,
+  srcTree, dstTree, parents, siblings, log, processedLink, processedDir,
 ) {
   return (entry) => {
     if (encodeURIComponent(entry.name) === indexName) {
-      return paddedName(entry.name, indentLevel);
+      return log.formattedEntry(entry.name);
     }
     const entryPath = path.join(srcTree, entry.name);
     if (entry.isFile()) {
       return fsPromises.symlink(entryPath, path.join(dstTree, entry.name))
-        .then(() => paddedName(entry.name, indentLevel));
+        .then(() => log.formattedEntry(entry.name));
     }
     if (entry.isSymbolicLink()) {
       return processedLink(entry.name)
-        .then(() => paddedName(entry.name, indentLevel));
+        .then(() => log.formattedEntry(entry.name));
     }
     if (entry.isDirectory()) {
-      return processedDir(entry.name, parents, siblings, indentLevel);
+      return processedDir(entry.name, parents, siblings, log);
     }
     return errorExit(`Cannot process ${entry.name} in ${srcTree}`);
   };
@@ -181,17 +228,17 @@ const webDirProcessor = function webDirRefProcessor(webTree) {
 };
 
 const dirProcessor = function directoryTreeProcessor(srcRoot, dstRoot, webRoot) {
-  const processedDir = (dirName, parents, siblings, indentLevel) => {
+  const processedDir = (dirName, parents, siblings, log) => {
     const webDirName = dirName;
     const nextParents = parents.concat([dirName]);
-    const nextIndent = indentLevel + 2;
+    const nextLog = log.deeper();
     const srcTree = joinedPath(srcRoot, nextParents);
     const dstTree = joinedPath(dstRoot, nextParents);
     const webAbove = [webRoot].concat(parents.map(encodeURIComponent)).join('/');
     const webTree = `${webAbove}/${webDirName}`;
     const navRefs = siblings.map(webNavProcessor(webAbove, dirName));
     const breadcrumbs = webBreadcrumbs(webRoot, parents);
-    const processedLink = linkProcessor(srcTree, dstTree);
+    const processedLink = linkProcessor(srcTree, dstTree, log);
     return fsPromises.mkdir(dstTree, { mode: 0o755 })
       .then(() => fsPromises.readdir(srcTree, { withFileTypes: true }))
       .then((rawEntries) => {
@@ -207,21 +254,20 @@ const dirProcessor = function directoryTreeProcessor(srcRoot, dstRoot, webRoot) 
           .map(dirEntry => dirEntry.name);
         return entries
           .map(entryProcessor(
-            srcTree, dstTree, nextParents, nextSiblings, nextIndent, processedLink, processedDir,
+            srcTree, dstTree, nextParents, nextSiblings, nextLog, processedLink, processedDir,
           ));
       })
       .then(dirList => Promise.all(dirList))
       .then((resolvedTree) => {
-        const dirHeader = `${paddedName(dirName, indentLevel)}/ ${parents.join(' --> ')} | [${siblings.join(', ')}]`;
-        return [dirHeader].concat(resolvedTree).join('\n');
+        log.formattedDir(dirName, parents, siblings, resolvedTree);
       })
-      .catch(errmes => logMessage(errmes));
+      .catch(errmes => log.message(errmes));
   };
   return processedDir;
 };
 
-const topLevelDir = function copiedTopLevelDataToDst(srcRoot, dstRoot, webRoot, processedDir) {
-  const processedLink = linkProcessor(srcRoot, dstRoot);
+const topLevelDir = function copiedTopLevelDataToDst(srcRoot, dstRoot, webRoot, log, processedDir) {
+  const processedLink = linkProcessor(srcRoot, dstRoot, log);
   return fsPromises.readdir(srcRoot, { withFileTypes: true })
     .then((rawEntries) => {
       const dirRefs = rawEntries.map(webDirProcessor(webRoot));
@@ -233,39 +279,39 @@ const topLevelDir = function copiedTopLevelDataToDst(srcRoot, dstRoot, webRoot, 
       const nextSiblings = entries.filter(entry => entry.isDirectory())
         .map(dirEntry => dirEntry.name);
       return entries
-        .map(entryProcessor(srcRoot, dstRoot, [], nextSiblings, 0, processedLink, processedDir));
+        .map(entryProcessor(srcRoot, dstRoot, [], nextSiblings, log, processedLink, processedDir));
     })
     .then(dirList => Promise.all(dirList))
     .then(resolvedTree => resolvedTree.join('\n'))
-    .catch(errmes => logMessage(errmes));
+    .catch(errmes => log.message(errmes));
 };
 
-const placedWebAssets = function copiedSelectiveWebAssetsToDst(assetRoot, dst) {
+const placedWebAssets = function copiedSelectiveWebAssetsToDst(assetRoot, dst, log) {
   const scriptDir = joinedPath(dst, webScripts);
   const imageDir = joinedPath(dst, webImages);
   const styleDir = joinedPath(dst, webStyles);
-  return patternCopiedFiles(assetRoot, dst, /\.ico$/)
+  return patternCopiedFiles(assetRoot, dst, /\.ico$/, log)
     .then(() => Promise.all(
       [scriptDir, imageDir, styleDir]
         .map(dir => fsPromises.mkdir(dir, { mode: 0o755, recursive: true })),
     ))
     .then(() => Promise.all([
-      patternCopiedFiles(assetRoot, scriptDir, /\.js$/),
-      patternCopiedFiles(assetRoot, imageDir, /\.((png)|(jpg)|(svg))$/),
-      patternCopiedFiles(assetRoot, styleDir, /\.css$/),
+      patternCopiedFiles(assetRoot, scriptDir, /\.js$/, log),
+      patternCopiedFiles(assetRoot, imageDir, /\.((png)|(jpg)|(svg))$/, log),
+      patternCopiedFiles(assetRoot, styleDir, /\.css$/, log),
     ]))
-    .catch(errmes => logMessage(errmes));
+    .catch(errmes => log.message(errmes));
 };
 
 const processedDirTree = dirProcessor(source, destination, webRootURL);
 
-purgedTree(destination)
+purgedTree(destination, topLog)
   .then(() => fsPromises.mkdir(destination, { mode: 0o755 }))
   .then(() => Promise.all([
-    placedWebAssets(assets, destination),
-    topLevelDir(source, destination, webRootURL, processedDirTree)
-      .then(dirLog => logMessage(dirLog)),
+    placedWebAssets(assets, destination, topLog),
+    topLevelDir(source, destination, webRootURL, topLog.deeper(), processedDirTree)
+      .then(dirLog => topLog.message(dirLog)),
   ]))
-  .then(() => relinkedTree(source, destination))
-  .then(linkLog => logMessage(linkLog))
-  .catch(errmes => logMessage(errmes));
+  .then(() => relinkedTree(source, destination, topLog))
+  .then(linkLog => topLog.message(linkLog))
+  .catch(errmes => topLog.message(errmes));
